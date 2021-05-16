0c9ab39835bb1ec21c7bc7a98b1dea72d1dfbdf4
Your Name <you@example.com>
2021-05-17 02:00:19 +0530

    Changing the database

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ const knex = require('knex') ;
 const db = knex({
     client: 'pg',
     connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'Anurag12#',
-    database : 'smartbrain'
+        connectionString : process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+      }
     }
 }) ;
 
